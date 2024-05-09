35e35c6be006ec3acb7b0e8645203f654e91e072
zugdev <zug.dev@proton.me>
2024-05-09 12:59:20 -0300

    feat: add send price to API in scraping script

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -22,4 +22,19 @@ def fetch_bitcoin_price():
     except requests.RequestException as e:
         print(f"Error fetching Bitcoin price: {e}")
 
+def fetch_and_send_bitcoin_price():
+    api_endpoint = "http://localhost:5000/submit_price"
+
+    try:
+        bitcoin_price = fetch_bitcoin_price()
+
+        # Send the price to our Flask API
+        send_response = requests.post(api_endpoint, json={"price": bitcoin_price})
+        send_response.raise_for_status()
+
+        print("Price sent successfully:", bitcoin_price)
+    except requests.RequestException as e:
+        print("Error:", e)
+
+fetch_and_send_bitcoin_price()
 fetch_bitcoin_price()
